0dba1e774da193324a229076c543e5cbc13bff60
Marina Lee <leemarina@gmail.com>
2021-07-15 13:50:55 -0700

    fast food :D

--- questview/ViewController.swift
+++ questview/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet weak var emojiButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    
 
+    @IBAction func burgerButton(_ sender: UIButton) {
+        emojiButton.setTitle("🍔", for: .normal)
+    }
+    
+    @IBAction func friesButton(_ sender: UIButton) {
+        emojiButton.setTitle("🍟", for: .normal)
+    }
+    
+    @IBAction func pizzaButton(_ sender: UIButton) {
+        emojiButton.setTitle("🍕", for: .normal)
 
+    }
+    
 }
 
